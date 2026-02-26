a79accab910c5d2bce16a787482f6f7b8b5aae9f
Talha8899 <talhaabdulsattar65@gmail.com>
2026-02-26 22:42:22 +0500

    add a programe in repo for find even odd numbers

---
+++ practice_even_odd.py
@@ -0,0 +1,8 @@
+#This programe take 1 number from user and tell the if the number
+# even or odd.
+
+number=int(input("enter the number:"))
+if (number%2==0):
+    print(number,"is even")
+else:
+    print(number,"is odd")
