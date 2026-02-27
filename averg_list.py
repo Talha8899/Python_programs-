69c2dfbeda4792adac179fd445b3cb8d42a2e7e0
Talha8899 <talhaabdulsattar65@gmail.com>
2026-02-27 10:35:16 +0500

    now it takes list from user

--- averg_list.py
+++ averg_list.py
@@ -1,9 +1,19 @@
 #this programe takes a list and return its aerage  
 
+#fuction for calculate average
 def list_averg(list):
    averg=sum(list)/len(list)
    print (averg)
    return averg
 
-my_list=[2,3,4,5,6]
-list_averg(my_list)
+#this part takes list elements from user as input
+mylist=[]
+while True:
+    mylist_items=input("enter the list values and write break when you done:")
+    if (mylist_items.lower()=="break"):
+        break
+    mylist.append(int(mylist_items))
+    print(mylist)
+
+# call for calculate averg
+list_averg(mylist)
