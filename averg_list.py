01fccd453d8acd80588dc240a52e41c6af196df0
Talha8899 <talhaabdulsattar65@gmail.com>
2026-02-27 09:56:33 +0500

    code for a list average

---
+++ averg_list.py
@@ -0,0 +1,10 @@
+#this programe takes a list and return its aerage  
+
+my_list=[2,3,4,5,6]
+list_averg(my_list)
+
+def list_averg(list):
+   averg=sum(list)/len(list)
+   print (averg)
+   return averg
+
