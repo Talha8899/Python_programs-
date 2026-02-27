dc9025fa5c9b5f37e31234692f0f1319866088c7
Talha8899 <talhaabdulsattar65@gmail.com>
2026-02-27 09:58:33 +0500

    correct mistake

--- averg_list.py
+++ averg_list.py
@@ -1,10 +1,9 @@
 #this programe takes a list and return its aerage  
 
-my_list=[2,3,4,5,6]
-list_averg(my_list)
-
 def list_averg(list):
    averg=sum(list)/len(list)
    print (averg)
    return averg
 
+my_list=[2,3,4,5,6]
+list_averg(my_list)
