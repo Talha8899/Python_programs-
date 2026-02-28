a1cdf5b83661cddfb3fe11a3a159fa67b229eca2
Talha8899 <talhaabdulsattar65@gmail.com>
2026-02-28 13:40:19 +0500

    add comment

--- my_class.py
+++ my_class.py
@@ -1,8 +1,10 @@
 # this class get a list and print avg
 class Student:
+    #init method
     def __init__(self,name,marks):
         self.name=name
         self.marks=marks
+        # method for print avg
     def get_avg(self):
         value=0
         for i in self.marks:
