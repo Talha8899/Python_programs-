8b1d94cd6c4b3d8f87c764b03f673d2f7d580946
Talha8899 <talhaabdulsattar65@gmail.com>
2026-02-28 16:01:35 +0500

    add a feature

--- banking.py
+++ banking.py
@@ -3,16 +3,19 @@ class Account:
     def __init__(self,acc_no,bal):
         self.acc_no=acc_no
         self.bal=bal
-        print(self.bal)
     #debit method
     def debit(self,bal):
         self.bal-=bal
+        print(bal,"was debit from your account")
         print("your remeaning balance is:",self.bal)
 
     #credit method
     def credit(self,bal):
         self.bal+=bal
         print("your balance is:",self.bal )
+    #balance check
+    def check_bal(self):
+        return print("your available balance is:",self.bal)
 
 s1=Account(332244,70000)
-s1.credit(10000)
+s1.debit(60000)
