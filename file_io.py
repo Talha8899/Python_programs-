4ded316b5737bf1ee70e277ad0a46f1b5b9d254f
Talha8899 <talhaabdulsattar65@gmail.com>
2026-03-07 15:48:07 +0500

    add comments

--- file_io.py
+++ file_io.py
@@ -1,7 +1,12 @@
+#open file and write data in it 
+#if file dosent exist it creat the file
 with open("myfile.txt","+w")as f:
     f.write("hi i am talha \n")
     f.write("i am learning file i/o operation using python")
+# add more data 
 with open("myfile.txt","+a")as f:
+    f.write("\nnice to meet you")
+#read data from file and print it 
+with open("myfile.txt")as f:
     data=f.read()
     print(data)
-    f.write("\nnice to meet you")
